6d7f7ac2fe256a39d5b0e85cf90cd506bcbca17e
Denis <denissm2002@yandex.ru>
2021-09-29 15:20:27 +0300

    added second comment

--- OIB_lab_2/OIB_lab_2/Form2.cs
+++ OIB_lab_2/OIB_lab_2/Form2.cs
@@ -13,6 +13,7 @@ namespace OIB_lab_2
     public partial class Form2 : Form
     {
         //Конструктор формы 2
+        //Просто коммент
         public Form2()
         {
             InitializeComponent();
