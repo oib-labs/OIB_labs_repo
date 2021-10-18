bfb004b9ec0387b60c6e88955b8adc594a0670d9
45gk <57001545+45gk@users.noreply.github.com>
2021-10-18 23:19:26 +0300

    Add files via upload

---
+++ InSec2_v2/InSec2_v2/Db.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InSec2_v2
+{
+    class Db
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=insec2_data;SSL Mode=None");
+        String login = "";
+        public void openConnection()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+        
+        public void writeLogin(String log)
+        {
+            login = log;
+        }
+
+        public String getLogin()
+        {
+            
+            return login;
+        }
+    }
+}
